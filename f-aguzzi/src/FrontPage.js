216e641540fac5bb105c25730f48b59d198f6e06
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-01-07 15:26:05 +0100

    Created page

---
+++ f-aguzzi/src/FrontPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FrontPage () {
+
+    return (
+        <div>
+            <h1 className="display">Federico Aguzzi</h1>
+        </div>
+    )
+}
